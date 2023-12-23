42769f4f92f23fb6bf83a5963052a823ad586a9a
Brian Duffey <bduff9@gmail.com>
2023-12-23 13:45:51 -0600

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs';
+import { Analytics } from '@vercel/analytics/react';
+
 import Header from '@/components/Header';
 
 import 'react-loading-skeleton/dist/skeleton.css';
@@ -16,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<ClerkProvider>
 					<Header />
 					{children}
+					<Analytics />
 				</ClerkProvider>
 			</body>
 		</html>
